33c43051b314c9c2c6f9c10780b28ceefe96b7a9
ciaran.mcmanus <ciaran.mcmanus@openjawtech.com>
2016-11-14 11:39:44 +0000

    V0.3.7 Adding folders so that MongoDB can run on CodeShip.

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
 var configDB   = require('./config/database.js');
 var HttpStatus = require('http-status-codes');
-//make sure promise deprecation warning is removed
-mongoose.Promise = global.Promise;
 //secure the application
 var helmet = require('helmet');
 var router 	   = express.Router();              // get an instance of the express Router
