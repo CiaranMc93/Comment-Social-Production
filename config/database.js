b17914eeab6f1343d9bfce9d482f5e49e58ff3bc
CiaranMc93 <C11354741@mydit.ie>
2016-11-26 10:27:32 +0000

    V1.0.0 Reverting to final latest commit. CI ipdated successfully.

--- config/database.js
+++ config/database.js
@@ -12,6 +12,6 @@ module.exports = {
 
 	//local database
     'urlLocal' : 'mongodb://localhost/user',
-    //production database
+    //production database correction
     'url' : 'mongodb://Ciacavus:ciaran@jello.modulusmongo.net:27017/Iriv7azu'
 };
